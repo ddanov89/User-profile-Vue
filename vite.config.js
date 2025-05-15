70167155907fa282aec18eaa3f8b530990c1d0f6
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-16 00:13:52 +0300

    Imports config adjustments

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,6 +10,14 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+      ],
+      dts: 'src/auto-imports.d.ts', // optional: generate TypeScript types
+    }),
   ],
   resolve: {
     alias: {
